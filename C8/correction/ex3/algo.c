60cd83e67d3491df4357f17a9e053d97949a4880
Julien Adam <adamj@paratools.com>
2022-03-15 16:56:57 +0100

    Update ex3

---
+++ C8/correction/ex3/algo.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <complex.h>
+#include <math.h>
+#include "ppm.h"
+
+
+#define TRSH 2.0
+#define ITER 1024ull
+
+#define SIZEX 2500
+#define SIZEY 2500
+
+double cx( int x )
+{
+	/* -2 ---> 1 */
+	static const double qx = 3.0 / (double)SIZEX;
+	return -2.0 +  x * qx;
+}
+
+double cy( int y )
+{
+	/* -1 ---> 1 */
+	static const double qy = 2.0 / (double)SIZEY;
+	return -1.0 + y * qy;
+}
+
+int main(int argc, char *argv[])
+{
+	struct ppm_image im;
+	ppm_image_init( &im , SIZEX , SIZEY );
+
+	int i,j;
+	double colref = 255.0/log(ITER);
+
+	for (i = 0; i < SIZEX; ++i) {
+		for (j = 0; j < SIZEY; ++j) {
+
+			unsigned long int iter = 0;
+
+			double complex c =  cx(i) + cy(j) * I;
+			double complex z = 0;
+
+			while(iter < ITER)
+			{
+				double mod = cabs(z);
+
+				if( TRSH < mod )
+				{
+					break;
+				}
+
+				z = z*z + c;
+
+				iter++;
+			}
+
+			struct col cc = getcol( log(iter), colref );
+			ppm_image_setpixel(&im, i,j, cc.r, cc.g , cc.b );
+		}
+	}
+
+	ppm_image_dump( &im, "m.ppm");
+	ppm_image_release( &im );
+
+	return 0;
+}
--- C8/correction/ex3/ppm.c
+++ C8/correction/ex3/ppm.c
@@ -1,59 +1,93 @@
 #include "ppm.h"
- 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
- 
- 
+
+
 int ppm_image_init( struct ppm_image *im , int w , int h )
 {
-    memset( im, 0, sizeof( struct ppm_image));
- 
-    im->width = w;
-    im->height = h;
- 
-    im->px = malloc( w * h * sizeof(struct ppm_pixel));
- 
-    if( !im->px)
-    {
-            perror("malloc");
-            return 1;
-        }
- 
-    return 0;
+	memset( im, 0, sizeof( struct ppm_image));
+
+	im->width = w;
+	im->height = h;
+
+	im->px = malloc( w * h * sizeof(struct ppm_pixel));
+
+	if( !im->px)
+	{
+		perror("malloc");
+		return 1;
+	}
+
+	return 0;
 }
- 
+
 int ppm_image_release( struct ppm_image *im )
 {
-    if( im == NULL )
-        return 1;
- 
-    free( im->px );
-    im->px = NULL;
- 
-    im->width = 0;
-    im->height = 0;
- 
-    return 0;
+	if( im == NULL )
+		return 1;
+
+	free( im->px );
+	im->px = NULL;
+
+	im->width = 0;
+	im->height = 0;
+
+	return 0;
 }
- 
+
 int ppm_image_dump( struct ppm_image *im, char * path )
 {
-    FILE * out = fopen( path , "w");
- 
-    if( !out )
-    {
-            perror("fopen");
-            return 1;
-        }
- 
-    fprintf(out, "P6\n");
-    fprintf(out, "%d\n", im->width);
-    fprintf(out, "%d\n", im->height);
-    fprintf(out, "255\n");
-    fwrite( im->px, sizeof(struct ppm_pixel) , im->width * im->height, out );
- 
-    fclose( out );
- 
-    return 0;
+	FILE * out = fopen( path , "w");
+
+	if( !out )
+	{
+		perror("fopen");
+		return 1;
+	}
+
+	fprintf(out, "P6\n");
+	fprintf(out, "%d\n", im->width);
+	fprintf(out, "%d\n", im->height);
+	fprintf(out, "255\n");
+	fwrite( im->px, sizeof(struct ppm_pixel) , im->width * im->height, out );
+
+	fclose( out );
+
+	return 0;
+}
+
+struct col getcol( int val , int max )
+{
+	double q = (double)val/(double)max;
+
+	struct col c = { 0, 0, 0 };
+
+	if( q < 0.25 )
+	{
+		c.r = ( q * 4.0 ) * 255.0;
+		c.b = 255;
+	}
+	else if( q < 0.5 )
+	{
+		c.b = 255;
+		c.g = 255;
+		c.r = (q-0.25)*4.0*255.0;
+
+	}
+	else if( q < 0.75 )
+	{
+		c.b = 255;
+		c.r = 255;
+		c.g = 255.0 - (q-0.5)*4.0*255.0;
+	}
+	else
+	{
+		c.b = 255-(q-0.75)*4.0*255.0;
+		c.g = 0;
+		c.r = 255;
+	}
+
+	return c;
 }
